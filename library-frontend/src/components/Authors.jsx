780654a9326f9823ab38837984bb785cb06be641
Hiwersen Angelo <100746220+hiwersen@users.noreply.github.com>
2026-09-07 18:10:48 -0300

    Chore: implement exercise 8

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,8 +1,13 @@
+import { useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS } from "../../queries";
+
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+  const result = useQuery(ALL_AUTHORS);
+
+  if (!props.show) return null;
+  if (result.loading) return <div>loading...</div>;
+
+  const authors = result.data.allAuthors;
 
   return (
     <div>
@@ -24,7 +29,7 @@ const Authors = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
